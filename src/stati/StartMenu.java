edab7098730e8fc72cdee8ab4cb62dd933ded871
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-03-27 16:32:37 +0100

    Fixata GUI del menu di start usando slick2d.MouseOverArea

--- src/stati/StartMenu.java
+++ src/stati/StartMenu.java
@@ -5,15 +5,21 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
-public class StartMenu extends BasicGameState {
+
+public class StartMenu extends BasicGameState implements ComponentListener {
     public static final int ID = 0;
     private GameContainer container;
     private Image background;
     private Image play;
+    private StateBasedGame stateBasedGame;
+    private MouseOverArea playButton;
 
 
     @Override
@@ -24,25 +30,25 @@ public class StartMenu extends BasicGameState {
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         this.container= gameContainer;
+        this.stateBasedGame= stateBasedGame;
         background= new Image("res/bg.jpeg");
-        play = new Image("res/play.png");
+        play = new Image("res/play.png").getScaledCopy(150, 70);
+        playButton = new MouseOverArea(container, play, 150, 250, 150, 70, this);
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         background.draw(0,0, container.getWidth(), container.getHeight());
-        play.draw(150,250,150,70);
+        playButton.render(gameContainer, graphics);
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-        int posX = Mouse.getX();
-        int posY = Mouse.getY();
 
-        if((posX>150&&posX<300)&&(posY>330&&posY<400)){
-            if(Mouse.isButtonDown(0)){
-                stateBasedGame.enterState(1);
-            }
+    }
+    public void componentActivated(AbstractComponent source) {
+        if (source == playButton ) {
+            stateBasedGame.enterState(1);
         }
     }
 }
